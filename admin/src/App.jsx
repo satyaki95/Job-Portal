95c0d3f7c8641f158f30c898ba8734d7d6b579c3
satyaki95 <www.satyaki95@gmail.com>
2026-08-17 23:34:31 +0530

    List job

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import AddJobs from "./pages/AddJobs";
+import ListJob from "./pages/ListJob";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/addjobs" element={<AddJobs />} />
+          <Route path="/list/jobs" element={<ListJob />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </div>
---
+++ admin/src/pages/ListJob.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import ListJobs from "../components/ListJobs";
+
+const ListJob = () => {
+  return (
+    <div>
+      <Navbar />
+      <ListJobs />
+    </div>
+  );
+};
+
+export default ListJob;
