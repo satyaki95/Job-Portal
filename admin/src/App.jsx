7e0ceae6d9e98ed1e506155edc981c46bd65e198
satyaki95 <www.satyaki95@gmail.com>
2026-08-19 01:27:15 +0530

    add company question

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import AddJobs from "./pages/AddJobs";
 import ListJob from "./pages/ListJob";
 import CompanyPage from "./pages/CompanyPage";
+import CompanyQuestion from "./pages/CompanyQuestion";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/addjobs" element={<AddJobs />} />
           <Route path="/list/jobs" element={<ListJob />} />
           <Route path="/companies" element={<CompanyPage />} />
+          <Route path="/company-questions" element={<CompanyQuestion />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </div>
