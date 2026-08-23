1c140c89b06b1d92c3d7dfd7f1064d099d3850e4
satyaki95 <www.satyaki95@gmail.com>
2026-08-23 20:27:49 +0530

    interview question and role banner

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar";
 import Banner from "../components/Banner";
 import Candidate from "../components/Candidate";
 import Career from "../components/Career";
+import InterviewQuestion from "../components/InterviewQuestion";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Banner />
       <Candidate />
       <Career />
+      <InterviewQuestion />
     </div>
   );
 };
